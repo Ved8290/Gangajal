6a1569ed5a92124c0e5f5664e9c8b43b39262b37
Ved8290 <133479514+Ved8290@users.noreply.github.com>
2025-01-22 00:12:29 +0530

    Update page.jsx

--- src/Payment/page.jsx
+++ src/Payment/page.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import './Payment.css';
+//import './Payment.css';
 
 const Payment = () => {
   const location = useLocation();
